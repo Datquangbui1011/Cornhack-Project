37f9a968056df6bc4c9f406277985f9e2bfbcc7a
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2025-03-02 01:16:50 -0600

    Fixing for deploy

--- side-quests-backend/build.sh
+++ side-quests-backend/build.sh
@@ -5,9 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Fetch Linux binaries (Render uses Ubuntu)
-prisma py fetch --platform linux --engine-version 5.17.0
-
 # Generate Prisma client
 prisma generate --schema=prisma/schema.prisma
 
